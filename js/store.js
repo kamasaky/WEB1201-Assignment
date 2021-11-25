9730350627e653cc1d09babd4322467f9238767d
Voronianskii Egor <vrnsky@protonmail.ch>
2021-11-25 13:56:35 +0800

    Cart is fully working

--- js/store.js
+++ js/store.js
@@ -12,6 +12,38 @@ function ready() {
         var button = removeCartItemButtons[i];
         button.addEventListener('click', removeCartItem);
     }
+
+    var addToCartButtons = document.getElementsByClassName("add-to-cart")
+    for (var i = 0; i < addToCartButtons.length; i++) {
+        var addToCartButton = addToCartButtons[i]
+        addToCartButton.addEventListener('click', addToCartClicked);
+    }
+
+}
+
+function addToCartClicked(event) {
+    var button = event.target;
+    var cartItem = button.parentElement;
+    var itemTitle = cartItem.getElementsByClassName("item-title")[0].innerText
+    var itemPrice = cartItem.getElementsByClassName("cost")[0].innerText.replace("RM", "")
+    var itemImage = cartItem.getElementsByClassName("item-img")[0].src;
+    addItemToCart(itemTitle, itemPrice, itemImage)
+}
+
+function addItemToCart(itemTitle, itemPrice, itemImage) {
+    var cartRow = document.createElement("div")
+    var cart = document.getElementsByClassName("cart")[0]
+    var cartRowContents = `
+    <div class="cart-entity">
+      <button class="remove-btn"><span class="fas fa-times"></span></button>
+      <img src="${itemImage}" alt="Mazero coffee">
+      <div class="product">
+        <h3>${itemTitle}</h3>
+        <div class="cost">RM ${itemPrice}</div>
+      </div>
+    </div>`
+    cartRow.innerHTML = cartRowContents
+    cart.append(cartRow)
 }
 
 function removeCartItem(event) {
